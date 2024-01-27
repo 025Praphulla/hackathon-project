d56515618739de8b251b66a735a3d3470b52c998
025Praphulla <023bscit025@sxc.edu.np>
2024-01-27 09:23:43 +0545

    Initials 5

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var map = L.map("map").setView([27.7017, 85.324], 13); // Centered on Maitighar, Kathmandu
+  var map = L.map("map").setView([27.7017, 85.324], 13);
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
   var pickupMarker = L.marker([0, 0], { draggable: false });
@@ -15,102 +14,85 @@ document.addEventListener("DOMContentLoaded", function () {
 
   var fare;
   var distance;
-  document
-    .getElementById("distanceForm")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      var pickup = document.getElementById("pickup").value + ", Kathmandu";
-      var destination =
-        document.getElementById("destination").value + ", Kathmandu";
 
-      Promise.all([
-        fetch(
-          "https://nominatim.openstreetmap.org/search?q=" +
-            pickup +
-            "&format=json&viewbox=85.277498,27.666667,85.453415,27.741809"
-        ).then((response) => response.json()),
-        fetch(
-          "https://nominatim.openstreetmap.org/search?q=" +
-            destination +
-            "&format=json&viewbox=85.277498,27.666667,85.453415,27.741809"
-        ).then((response) => response.json()),
-      ])
-        .then(function (responses) {
-          var pickupData = responses[0];
-          var destinationData = responses[1];
+  document.getElementById("distanceForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    var pickup = document.getElementById("pickup").value + ", Kathmandu";
+    var destination = document.getElementById("destination").value + ", Kathmandu";
 
-          var pickupLat = pickupData[0].lat;
-          var pickupLon = pickupData[0].lon;
-          var destinationLat = destinationData[0].lat;
-          var destinationLon = destinationData[0].lon;
+    Promise.all([
+      fetch(
+        "https://nominatim.openstreetmap.org/search?q=" +
+          pickup +
+          "&format=json&viewbox=85.277498,27.666667,85.453415,27.741809"
+      ).then((response) => response.json()),
+      fetch(
+        "https://nominatim.openstreetmap.org/search?q=" +
+          destination +
+          "&format=json&viewbox=85.277498,27.666667,85.453415,27.741809"
+      ).then((response) => response.json()),
+    ])
+      .then(function (responses) {
+        var pickupData = responses[0];
+        var destinationData = responses[1];
 
-          pickupMarker.setLatLng([pickupLat, pickupLon]).addTo(map);
-          destinationMarker
-            .setLatLng([destinationLat, destinationLon])
-            .addTo(map);
+        var pickupLat = pickupData[0].lat;
+        var pickupLon = pickupData[0].lon;
+        var destinationLat = destinationData[0].lat;
+        var destinationLon = destinationData[0].lon;
 
-          var waypoints = [
-            L.latLng(pickupLat, pickupLon),
-            L.latLng(destinationLat, destinationLon),
-          ];
+        pickupMarker.setLatLng([pickupLat, pickupLon]).addTo(map);
+        destinationMarker.setLatLng([destinationLat, destinationLon]).addTo(map);
 
-          control.setWaypoints(waypoints);
+        var waypoints = [
+          L.latLng(pickupLat, pickupLon),
+          L.latLng(destinationLat, destinationLon),
+        ];
 
-          distance =
-            pickupMarker.getLatLng().distanceTo(destinationMarker.getLatLng()) /
-            1000; // in kilometers
+        control.setWaypoints(waypoints);
 
-          // Calculate fare based on distance
-          if (distance <= 5) {
-            fare = 20;
-          } else if (distance > 5 && distance <= 10) {
-            fare = 25;
-          } else {
-            fare = 30;
-          }
+        distance =
+          pickupMarker.getLatLng().distanceTo(destinationMarker.getLatLng()) /
+          1000; // in kilometers
 
-          // Display the result and QR code divs
-          document.getElementById("result").innerHTML =
-            "Distance: " + distance.toFixed(2) + " km <br> Fare: NPR " + fare;
+        if (distance <= 5) {
+          fare = 20;
+        } else if (distance > 5 && distance <= 10) {
+          fare = 25;
+        } else {
+          fare = 30;
+        }
 
-          // Call generateQR with the calculated fare amount
-          generateQR(fare);
+        document.getElementById("result").innerHTML =
+          "Distance: " + distance.toFixed(2) + " km <br> Fare: NPR " + fare;
 
-          // Hide form elements
-          document.getElementById("distanceForm").style.display = "none";
+        generateQR(fare);
 
-          // Display the result and QR code divs
-          document.getElementById("result").style.display = "block";
-          document.getElementById("qr-code").style.display = "block";
-
-          // scanQR(fare);
-        })
-        .catch(function (error) {
-          console.error("Error:", error);
-        });
-    });
+        document.getElementById("distanceForm").style.display = "none";
+        document.getElementById("result").style.display = "block";
+        document.getElementById("qr-code").style.display = "block";
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  });
 
   function generateQR(fareAmount) {
-    // Assuming you have a container with the id "qr-code" in your HTML where you want to display the QR code
     var qrCode = new QRCode("qr-code", {
-      text: "reciept.html", // Fix the typo in the URL
+      text: "reciept",
       width: 128,
       height: 128,
     });
 
-    // Get the QR code container element
     var qrCodeContainer = document.getElementById("qr-code");
 
-    // Add a click event listener to the container
     qrCodeContainer.addEventListener("click", function () {
-      // Replace "try.html" with the actual URL you want to open in the same tab
       document.getElementById("result").style.display = "none";
       document.getElementById("qr-code").style.display = "none";
 
       scanQR(fare);
       var newElement = document.createElement("div");
 
-      // Set content for the new element
       newElement.innerHTML =
         "Distance: " +
         distance.toFixed(2) +
@@ -123,10 +105,8 @@ document.addEventListener("DOMContentLoaded", function () {
         "<br> Remaining Balance: Rs " +
         balance;
 
-      // Add a class to the new element
       newElement.classList.add("custom-result");
 
-      // Append the new element to the document body or any other parent element
       document.body.appendChild(newElement);
     });
   }
@@ -134,11 +114,8 @@ document.addEventListener("DOMContentLoaded", function () {
   var balance = 500;
   document.getElementById("user-balance").innerHTML = "Balance: Rs " + balance;
 
-  // Function to simulate scanning QR code and update balance
   function scanQR(fareAmount) {
-    // Simulate processing transaction and updating balance
     balance -= fareAmount;
-    document.getElementById("user-balance").innerHTML =
-      "Balance: Rs " + balance;
+    document.getElementById("user-balance").innerHTML = "Balance: Rs " + balance;
   }
 });
